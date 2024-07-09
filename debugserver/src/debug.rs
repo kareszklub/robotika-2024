290fad367696787f34a89025fc71bfca725f94f8
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2024-07-09 02:52:26 +0200

    Implement message backend for debug server

--- debugserver/src/debug.rs
+++ debugserver/src/debug.rs
@@ -1,37 +1,70 @@
+use anyhow::anyhow;
 use tokio::{
-    io::AsyncReadExt,
+    io::{AsyncReadExt, BufStream},
     net::{TcpListener, TcpStream},
-    sync::mpsc::UnboundedSender,
+    sync::mpsc::{UnboundedReceiver, UnboundedSender},
 };
 
-type Tx = UnboundedSender<String>;
+use crate::message::{Control, ControlValue, Message};
 
-pub async fn init(tx: Tx) -> anyhow::Result<()> {
+type Tx = UnboundedSender<Message>;
+type Rx = UnboundedReceiver<ControlValue>;
+
+pub async fn init(mut tx: Tx, mut rx: Rx) -> anyhow::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:9999").await?;
+
     loop {
         let (socket, _) = listener.accept().await?;
 
-        if let Err(e) = process_socket(socket, tx.clone()).await {
+        if let Err(e) = process_socket(socket, &mut tx, &mut rx).await {
             error!("error while processing debug socket: {e}");
         }
     }
 }
 
-async fn process_socket(mut socket: TcpStream, tx: Tx) -> anyhow::Result<()> {
+async fn process_socket(socket: TcpStream, tx: &mut Tx, rx: &mut Rx) -> anyhow::Result<()> {
+    let mut socket = BufStream::new(socket);
+
     let mut buf = Vec::new();
     loop {
         let mtype = socket.read_u8().await?;
+
         match mtype {
             // 1: debug message
             1 => {
                 let len = socket.read_u16().await? as usize;
+
                 buf.resize(len, 0);
                 socket.read_exact(&mut buf).await?;
+
                 let msg = String::from_utf8(buf.clone())?;
                 debug!("msg: {msg}");
-                tx.send(msg)?;
+
+                tx.send(Message::Print(msg))?;
             }
-            _ => (),
+
+            // 2: define controls
+            2 => {
+                let len = socket.read_u16().await? as usize;
+                let mut ctrls = Vec::with_capacity(len);
+
+                for _ in 0..len {
+                    ctrls.push(Control::read(&mut socket).await?);
+                }
+
+                tx.send(Message::DefineControls(ctrls))?;
+            }
+
+            // 3: get control value
+            3 => {
+                let c = socket.read_u8().await?;
+                tx.send(Message::GetControlValue(c))?;
+
+                let v = rx.recv().await.ok_or(anyhow!("No response??"))?;
+                v.write(&mut socket).await?;
+            }
+
+            t => Err(anyhow!("Invalid message type {t}"))?,
         }
     }
 }
---
+++ debugserver/src/message.rs
@@ -0,0 +1,93 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+pub enum Control {
+    Bool { default: bool },
+    Float { default: f32, min: f32, max: f32 },
+    Int { default: i64, min: i64, max: i64 },
+    String { default: String },
+}
+impl Control {
+    pub async fn read<A>(w: &mut A) -> tokio::io::Result<Self>
+    where
+        A: AsyncReadExt + Unpin,
+    {
+        let t = w.read_u8().await?;
+        Ok(match t {
+            // bool:
+            0 => {
+                let default = w.read_u8().await? != 0;
+                Self::Bool { default }
+            }
+
+            // float:
+            1 => {
+                let default = w.read_f32().await?;
+                let min = w.read_f32().await?;
+                let max = w.read_f32().await?;
+                Self::Float { default, min, max }
+            }
+
+            // int:
+            2 => {
+                let default = w.read_i64().await?;
+                let min = w.read_i64().await?;
+                let max = w.read_i64().await?;
+                Self::Int { default, min, max }
+            }
+
+            // string:
+            3 => {
+                let len = w.read_u16().await? as usize;
+
+                let mut buf = vec![0; len];
+                w.read_exact(&mut buf).await?;
+
+                let default = String::from_utf8(buf).map_err(|_| {
+                    tokio::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        format!("Invalid control type {t}"),
+                    )
+                })?;
+
+                Self::String { default }
+            }
+
+            _ => Err(tokio::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Invalid control type {t}"),
+            ))?,
+        })
+    }
+}
+
+pub enum Message {
+    Print(String),
+    DefineControls(Vec<Control>),
+    GetControlValue(u8),
+}
+
+pub enum ControlValue {
+    Bool(bool),
+    Float(f32),
+    Int(i64),
+    String(String),
+}
+
+impl ControlValue {
+    pub async fn write<A>(&self, w: &mut A) -> tokio::io::Result<()>
+    where
+        A: AsyncWriteExt + Unpin,
+    {
+        match self {
+            ControlValue::Bool(v) => w.write_all(&(*v as u8).to_be_bytes()).await?,
+            ControlValue::Float(v) => w.write_all(&v.to_be_bytes()).await?,
+            ControlValue::Int(v) => w.write_all(&v.to_be_bytes()).await?,
+            ControlValue::String(v) => {
+                w.write_all(&(v.len() as u16).to_be_bytes()).await?;
+                w.write_all(v.as_bytes()).await?
+            }
+        }
+
+        Ok(())
+    }
+}
