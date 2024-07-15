3995a382f28c13918d43140077c8f9bd48c9052f
beni69 <73666440+beni69@users.noreply.github.com>
2024-07-15 16:53:32 +0200

    restructure internal state and channels, add config diffing

--- debugserver/src/debug.rs
+++ debugserver/src/debug.rs
@@ -1,26 +1,28 @@
-use crate::message::Control;
+use crate::{message::Control, AppState, SseMessage};
 use anyhow::anyhow;
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    sync::Arc,
+};
 use tokio::{
     io::{AsyncReadExt, BufStream},
     net::{TcpListener, TcpStream},
 };
 
-type Tx = tokio::sync::broadcast::Sender<String>;
-
-pub async fn init(mut tx: Tx) -> anyhow::Result<()> {
+pub async fn init(state: Arc<AppState>) -> anyhow::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:9999").await?;
 
     loop {
         let (socket, _) = listener.accept().await?;
 
         // will "block" this loop for the socket, meaning maximum one pico will be handled at any time
-        if let Err(e) = process_socket(socket, &mut tx).await {
+        if let Err(e) = process_socket(socket, &state).await {
             error!("error while processing debug socket: {e}");
         }
     }
 }
 
-async fn process_socket(socket: TcpStream, tx: &mut Tx) -> anyhow::Result<()> {
+async fn process_socket(socket: TcpStream, state: &AppState) -> anyhow::Result<()> {
     let mut socket = BufStream::new(socket);
     let mut buf = Vec::new();
 
@@ -38,19 +40,37 @@ async fn process_socket(socket: TcpStream, tx: &mut Tx) -> anyhow::Result<()> {
                 let msg = String::from_utf8(buf.clone())?;
                 debug!("msg: {msg}");
 
-                tx.send(msg)?;
+                state.msg_tx.send(SseMessage::Log(msg))?;
             }
 
             // 2: define controls
             2 => {
                 let len = socket.read_u16().await? as usize;
-                let mut ctrls = Vec::with_capacity(len);
+                // let mut ctrls = Vec::with_capacity(len);
+                // for _ in 0..len {
+                //     ctrls.push(Control::read(&mut socket).await?);
+                // }
 
-                for _ in 0..len {
-                    ctrls.push(Control::read(&mut socket).await?);
+                // TODO: construct hashmap from socket
+                let ctrls: HashMap<String, Control> = HashMap::new();
+
+                let mut hs = state.config.write().await;
+
+                // filter out removed values
+                hs.retain(|k, _| ctrls.contains_key(k));
+
+                for (name, control) in ctrls {
+                    match hs.entry(name) {
+                        // new value introduced on the pico
+                        Entry::Vacant(entry) => {
+                            entry.insert(control);
+                        }
+                        // override already exists
+                        Entry::Occupied(_) => {
+                            // TODO: send 0x01 to pico
+                        }
+                    };
                 }
-
-                // TODO: handle unprompted controls
             }
 
             t => Err(anyhow!("Invalid message type {t}"))?,
