2cc7a84111816ceea157c753bebfdd895824fe9b
beni69 <73666440+beni69@users.noreply.github.com>
2024-07-15 19:55:46 +0200

    riced out test script

--- debugserver/test.py
+++ debugserver/test.py
@@ -5,24 +5,41 @@ import socket, struct, time
 HOST = "127.0.0.1"
 PORT = 9999
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    return s
 
-def send(_msg: str):
+def send(s: socket.socket, _msg: str):
+    print(_msg)
     msg = _msg.encode("utf-8")
     s.sendall(b"\x01")
     s.sendall(struct.pack("!H", len(msg)))
     s.sendall(msg)
 
+s = connect()
+conn = True
 
 # send("buffher?")
 # send("i barely know her")
 
-i = 0
 while True:
-    send(f"bruh moment numero {i}")
-    i += 1
-    time.sleep(1)
+    try:
 
+        i = 0
+        while True:
+            send(s, f"bruh moment numero {i}")
+            i += 1
+            time.sleep(1)
 
-s.close()
+    except socket.error:
+        conn = False
+        print("[disconnected]")
+        while not conn:
+            try:
+                s = connect()
+                conn = True
+                print("[reconnected]")
+            except socket.error:
+                print("[reconnect failed]")
+                time.sleep(1)
