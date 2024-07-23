8b5be52e031e7172d640a242776e704d5d32a9ae
beni69 <73666440+beni69@users.noreply.github.com>
2024-07-24 01:20:54 +0200

    🦅🦅🦅 WHAT THE FUCK IS A FLEXBOX ⁉️⁉️

--- debugserver/index.js
+++ debugserver/index.js
@@ -1,30 +1,22 @@
 window.htmx = require('htmx.org').default;
 require("htmx-ext-sse");
 
-const numWidth = number => (number.style.width = 6 + Math.max(3, number.value.toString().length) + "ch")
-
 const onload = () => {
     document.body.querySelectorAll("input[type=range]").forEach(range => {
-        let number = range.parentElement.querySelector("input[type=number]");
+        let number = range.parentElement.parentElement.querySelector("input[type=number]");
 
         range.addEventListener("input", ()=>{
             number.value = range.value;
-
-            numWidth(number);
         });
     });
 
     document.body.querySelectorAll("input[type=number]").forEach(number => {
         let range = number.parentElement.querySelector("input[type=range]");
 
-        number.addEventListener("input", () => numWidth(number));
-
         number.addEventListener("change", () => {
             range.value = number.value;
             range.dispatchEvent(new Event('change'));
         });
-
-        numWidth(number);
     });
 };
 
