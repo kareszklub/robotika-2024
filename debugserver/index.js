60a59b7c5046dc643415ce3c90d7d6de59bd8a82
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2024-07-26 05:47:08 +0200

    Add fast input mode, remove test.py
    I removed test.py because it was a duplicated implementation of
    code/debug.py and code/networking.py.

--- debugserver/index.js
+++ debugserver/index.js
@@ -1,25 +1,40 @@
 window.htmx = require('htmx.org').default;
-require("htmx-ext-sse");
+require('htmx-ext-sse');
 
 const onload = () => {
-    document.body.querySelectorAll("input[type=range]").forEach(range => {
-        let number = range.parentElement.parentElement.querySelector("input[type=number]");
+    document.body.querySelectorAll('input[type=range]').forEach(range => {
+        let number = range.parentElement.parentElement.querySelector('input[type=number]');
 
-        range.addEventListener("input", ()=>{
+        range.addEventListener('input', () => {
             number.value = range.value;
         });
     });
 
-    document.body.querySelectorAll("input[type=number]").forEach(number => {
-        let range = number.parentElement.querySelector("input[type=range]");
+    document.body.querySelectorAll('input[type=number]').forEach(number => {
+        let range = number.parentElement.querySelector('input[type=range]');
 
-        number.addEventListener("change", () => {
+        number.addEventListener('change', () => {
             range.value = number.value;
             range.dispatchEvent(new Event('change'));
         });
     });
+
+    document.body.querySelectorAll('input[type=checkbox][name=fast]').forEach(fast => {
+        let inputs = Array.from(fast.parentElement.querySelectorAll('input').values())
+            .filter(e => e != fast && e.type != 'checkbox');
+
+        if (inputs.length == 0)
+            return;
+
+        fast.addEventListener('change', () => {
+            for (const inp of inputs)
+                inp.setAttribute('hx-trigger', fast.checked ? 'input throttle:10ms' : 'change');
+
+            htmx.process(fast.parentElement);
+        });
+    });
 };
 
-document.addEventListener("DOMContentLoaded", onload);
+document.addEventListener('DOMContentLoaded', onload);
 
-document.addEventListener("htmx:load", e => e.detail.elt.id === "controls" && onload());
+document.addEventListener('htmx:load', e => e.detail.elt.id === 'controls' && onload());
