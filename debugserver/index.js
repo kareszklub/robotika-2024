5ce979117f531e0c401e0c2afc5421e08563a74c
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2024-07-29 10:55:46 +0200

    Clean up fast mode

--- debugserver/index.js
+++ debugserver/index.js
@@ -2,39 +2,52 @@ window.htmx = require('htmx.org').default;
 require('htmx-ext-sse');
 
 const onload = () => {
-    document.body.querySelectorAll('input[type=range]').forEach(range => {
-        let number = range.parentElement.parentElement.querySelector('input[type=number]');
+    document.body.querySelectorAll('.control-container').forEach(ctrl => {
+        // all inputs and fast checkboxes, except regular checkboxes
+        const inputs = Array.from(ctrl.querySelectorAll('input'))
+            .filter(e => !(e.type === 'checkbox' && e.name !== 'fast'));
 
-        range.addEventListener('input', () => {
-            number.value = range.value;
-        });
-    });
+        const fast = inputs.find(e => e.type === 'checkbox' && e.name === 'fast');
+        const number = inputs.find(e => e.type === 'number');
+        const range = inputs.find(e => e.type === 'range');
 
-    document.body.querySelectorAll('input[type=number]').forEach(number => {
-        let range = number.parentElement.querySelector('input[type=range]');
+        if (number && range) {
+            range.addEventListener('input', () => { number.value = range.value; });
 
-        number.addEventListener('change', () => {
-            range.value = number.value;
-            range.dispatchEvent(new Event('change'));
-        });
-    });
+            // so we can access it later
+            number.onchange = () => {
+                range.value = number.value;
+                range.dispatchEvent(new Event(fast.checked ? 'input' : 'change'));
+            };
+        }
 
-    document.body.querySelectorAll('input[type=checkbox][name=fast]').forEach(fast => {
-        let inputs = Array.from(fast.parentElement.querySelectorAll('input').values())
-            .filter(e => e != fast && e.type != 'checkbox');
-
-        if (inputs.length == 0)
-            return;
-
-        fast.addEventListener('change', () => {
-            for (const inp of inputs)
-                inp.setAttribute('hx-trigger', fast.checked ? 'input throttle:10ms' : 'change');
-
-            htmx.process(fast.parentElement);
-        });
+        // at least fast and one other
+        if (fast && inputs.length > 1) {
+            fast.addEventListener('change', () => {
+                for (const inp of inputs) {
+                    if (inp.type === 'number') {
+                        // swap onchange and oninput
+                        [inp.onchange, inp.oninput] = [inp.oninput, inp.onchange];
+                    } else if (inp.name !== 'fast') {
+                        inp.setAttribute('hx-trigger', fast.checked ? 'input throttle:10ms' : 'change');
+                        htmx.process(inp);
+                    }
+                }
+            });
+        }
     });
 };
 
-document.addEventListener('DOMContentLoaded', onload);
+const MAX_ELEMENTS = 500;
+document.addEventListener('DOMContentLoaded', () => {
+    const logs = document.getElementById('logs');
+
+    logs.addEventListener('htmx:afterSwap', () => {
+        if (logs.childElementCount >= MAX_ELEMENTS)
+            logs.removeChild(logs.children[0]);
+    });
+
+    onload();
+});
 
 document.addEventListener('htmx:load', e => e.detail.elt.id === 'controls' && onload());
